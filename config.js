4e0c49f7d08e95704459f1a1fd6f71522cf311cb
MoizBabinwale <mbabinwale@gmail.com>
2023-01-30 15:58:27 +0530

    config

---
+++ config.js
@@ -0,0 +1,6 @@
+export default {
+    JWT_SECRET : "abc1234",
+    EMAIL:"cyrus.oreilly49@ethereal.email"  , // testing email & password
+    PASSWORD : "rEbZK9XvyyXJSqB7y4",
+    ATLAS_URI: "mongodb+srv://Moizbabinwale:moizbabinwala@auth.kswsbgj.mongodb.net/?retryWrites=true&w=majority"
+}
